aba3d57f5e692f47c980f54a826d8c1320c1b24a
ShadiJam <shadijam00@gmail.com>
2017-01-19 15:16:39 -0600

    adds correct user model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118182606) do
+ActiveRecord::Schema.define(version: 20170119211541) do
 
   create_table "responses", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.boolean  "attending"            null: false
+    t.boolean  "attending"
     t.integer  "number_of_attendees"
     t.text     "food_allergies"
     t.datetime "created_at",          null: false
@@ -27,6 +27,9 @@ ActiveRecord::Schema.define(version: 20170118182606) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -37,8 +40,6 @@ ActiveRecord::Schema.define(version: 20170118182606) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
